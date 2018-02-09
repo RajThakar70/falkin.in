44525e3ecaaf1f41057aabfdb35c299bbe3cb63d
RajThakar70 <rajthakar31@gmail.com>
2018-02-10 03:03:46 +0530

    Removed animation

--- client/components/LoginForm/index.js
+++ client/components/LoginForm/index.js
@@ -20,14 +20,12 @@ export default class LoginForm extends Component{
           <Grid style={{height:500}} verticalAlign='middle' centered columns={3}>
             <Grid.Row stretched>
               <Grid.Column>
-                <Transition animation={"fade"} duration={1000} visible={this.state.visible}>
-                  <Form inverted>
-                    <Form.Input icon="user" iconPosition="left" fluid label='User Name' placeholder='User Name' />
-                    <Form.Input icon='lock' iconPosition='left' fluid label='Password' type="password" placeholder='Password' />
-                    <Button onClick={()=>this.toggleVisibility()} floated='right' inverted fluid color='green'>Login</Button>
-                  </Form>
-                </Transition>
-                </Grid.Column>
+                <Form inverted>
+                  <Form.Input icon="user" iconPosition="left" fluid label='User Name' placeholder='User Name' />
+                  <Form.Input icon='lock' iconPosition='left' fluid label='Password' type="password" placeholder='Password' />
+                  <Button onClick={()=>this.toggleVisibility()} floated='right' inverted fluid color='green'>Login</Button>
+                </Form>
+              </Grid.Column>
               </Grid.Row>
             </Grid>
         </Segment>
