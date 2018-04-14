de2de32804215c3873dc04a5fee62a3476024d9c
adityapsvs <adityapsvs@gmail.com>
2018-04-14 10:11:35 +0530

    Small changes in style

--- client/components/Appbar/DesktopContainer.js
+++ client/components/Appbar/DesktopContainer.js
@@ -16,6 +16,7 @@ import {
 } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 import { HomepageHeading } from './HomepageHeading';
+import './index.css'
 
 export default class DesktopContainer extends Component {
   state = { activeItem: window.location.pathname}
@@ -27,11 +28,14 @@ export default class DesktopContainer extends Component {
   render() {
     const { children } = this.props
     const { fixed, activeItem } = this.state
+    // const menuStyle = {
+    //   fontFamily: 'Palanquin', sans-serif;
+    // }
 
     return (
       <Responsive {...Responsive.onlyComputer}>
         <Visibility once={false} onBottomPassed={this.showFixedMenu} onBottomPassedReverse={this.hideFixedMenu}>
-          <Segment inverted textAlign='center' style={{ minHeight: 300, padding: '1em 0em' }} vertical>
+          <Segment inverted textAlign='center' style={{ minHeight: 50, padding: '1em 0em' }} vertical>
             <Menu
               fixed={fixed ? 'top' : null}
               inverted
@@ -40,12 +44,12 @@ export default class DesktopContainer extends Component {
               size='large'
             >
               <Container>
-                <Menu.Item name='/' as={Link} to="/" active={activeItem === '/'} onClick={this.handleItemClick}>Home</Menu.Item>
-                <Menu.Item name='/about-us' as={Link} to="/about-us" active={activeItem === '/about-us'} onClick={this.handleItemClick}>About us</Menu.Item>
-                <Menu.Item name='/contact' as={Link} to='/contact' active={activeItem === '/contact'} onClick={this.handleItemClick}>Contact</Menu.Item>
-                <Menu.Item name='/products' as={Link} to='/products' active={activeItem === '/products'} onClick={this.handleItemClick}>Products</Menu.Item>
-                <Menu.Item name='/devices' as={Link} to="/devices" active={activeItem === '/devices'} onClick={this.handleItemClick}>Devices</Menu.Item>
-                <Menu.Item name='/login' as={Link} to="/login" active={activeItem === '/login'} position='right' onClick={this.handleItemClick}>Log in</Menu.Item>
+                <Menu.Item className="DesktopMenu" name='/' as={Link} to="/" color={activeItem === '/' ? "blue":"none"} active={activeItem === '/'} onClick={this.handleItemClick}>Home</Menu.Item>
+                <Menu.Item className="DesktopMenu" name='/about-us' as={Link} to="/about-us" color={activeItem === '/about-us' ? "blue":"none"} active={activeItem === '/about-us'} onClick={this.handleItemClick}>About us</Menu.Item>
+                <Menu.Item className="DesktopMenu" name='/contact' as={Link} to='/contact' color={activeItem === '/contact' ? "blue":"none"} active={activeItem === '/contact'} onClick={this.handleItemClick}>Contact</Menu.Item>
+                <Menu.Item className="DesktopMenu" name='/products' as={Link} to='/products' color={activeItem === '/products' ? "blue":"none"} active={activeItem === '/products'} onClick={this.handleItemClick}>Products</Menu.Item>
+                <Menu.Item className="DesktopMenu" name='/devices' as={Link} to="/devices"color={activeItem === '/devices' ? "blue":"none"}  active={activeItem === '/devices'} onClick={this.handleItemClick}>Devices</Menu.Item>
+                <Menu.Item className="DesktopMenu" name='/login' as={Link} to="/login" color={activeItem === '/login' ? "blue":"none"} active={activeItem === '/login'} position='right' onClick={this.handleItemClick}>Log in</Menu.Item>
               </Container>
             </Menu>
             {window.location.pathname==='/'?<HomepageHeading />:null}
---
+++ client/components/Products/index.js
@@ -0,0 +1,102 @@
+import React, { Component } from 'react';
+import {
+  Button,
+  Container,
+  Divider,
+  Grid,
+  Header,
+  Icon,
+  Image,
+  List,
+  Menu,
+  Responsive,
+  Segment,
+  Sidebar,
+  Visibility,
+} from 'semantic-ui-react';
+import PeopleCounter from '../../assets/images/PeopleCounter.png'
+
+
+export default class Home extends Component{
+  render(){
+    return(
+    <ProductsPageLayout />
+    )
+  }
+}
+
+const ProductsPageLayout = () => (
+  <div>
+    <Segment style={{ padding: '8em 0em' }} vertical>
+      <Grid container stackable verticalAlign='middle'>
+        <Grid.Row textAlign='center'>
+          <Grid.Column width={8}>
+            <Header as='h3' style={{ fontSize: '2em' }}>Falkin Technologies launching one of it’s products: </Header>
+            <Header as='h1' style={{ fontSize: '2em' }}>The people counter</Header>
+          </Grid.Column>
+          <Grid.Column floated='right' width={6}>
+            <Image
+              bordered
+              rounded
+              size='large'
+              src={PeopleCounter}
+            />
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </Segment>
+    <Segment style={{ padding: '0em' }} vertical>
+      <Grid celled='internally' columns='equal' stackable>
+        <Grid.Row>
+          <Grid.Column textAlign='left' style={{ paddingBottom: '5em', paddingTop: '5em' }}>
+            <Header as='h3' style={{ fontSize: '2em' }}>"The people counter"</Header>
+            <p style={{ fontSize: '1.33em' }}>
+              - Footfalls in your showroom <br />
+              - The time analysis of when the footfalls happened<br />
+              - Daily mailed report of complete analysis of the days football<br />
+              - Website to keep track of all analysis to keep track of your performance.<br />
+            </p>
+          </Grid.Column>
+          <Grid.Column textAlign='center' style={{ paddingBottom: '5em', paddingTop: '5em' }}>
+            <Header as='h3' style={{ fontSize: '2em' }}>"I want my country to be a superpower and make science the axis upon which
+            India runs."</Header>
+            <p style={{ fontSize: '1.33em' }}>
+              <Icon circular size='large' color='grey' name='user circle' />
+              <b>{' '}Moxad Thaker</b> Founder
+            </p>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </Segment>
+    <Segment style={{ padding: '8em 0em' }} vertical>
+      <Container text>
+        <Header as='h3' style={{ fontSize: '2em' }}>The people counter, More...</Header>
+        <p style={{ fontSize: '1.33em' }}>
+          In today’s time with the entire world shifting towards the smart era, it has become of
+          utmost importance to understand how your brand is performing with its audience. With
+          the online front presented to the general public through websites, it is being closely
+          watching by Search Engine Optimization and it is very important to track the number of
+          people visiting your brand. Hence our product cover the other equally important
+          segment of watching your customers, which are offline.
+        </p>
+        <Divider
+          as='h4'
+          className='header'
+          horizontal
+          style={{ margin: '3em 0em', textTransform: 'uppercase' }}
+        >
+          <div>Case Study</div>
+        </Divider>
+        <Header as='h3' style={{ fontSize: '2em' }}>Why you need "People Counter"?</Header>
+        <p style={{ fontSize: '1.33em' }}>
+          At the offline level, the customers who have made it to your respected
+          showroom/shop/business are there to really consider to make purchases of your
+          particular brand. Hence keeping a track of your customers who are going to be potential
+          direct clients becomes necessary.
+        </p>
+        <Button as='a' size='large'>I'm Still Quite Interested</Button>
+      </Container>
+    </Segment>
+
+  </div>
+)
